a3de9852a031e6e4d9e8ef4af571c6be92f370f1
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-24 16:47:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
 
   def kind
     illegal
-    elsif side1 == side2 && side2 == side3
+    if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
       :isosceles
